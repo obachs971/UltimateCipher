a93f252b6d68522bbb7cc5f4dbabfd7312c53fb7
Timwi <github@timwi.de>
2021-10-22 12:35:04 +0200

    Code overhaul and de-duplication

---
+++ Assets/Scripts/CipherResult.cs
@@ -0,0 +1,14 @@
+﻿namespace UltimateCipher
+{
+    public sealed class CipherResult
+    {
+        public string Answer { get; private set; }
+        public PageInfo[] Pages { get; private set; }
+
+        public CipherResult(string answer, PageInfo[] pages)
+        {
+            Answer = answer;
+            Pages = pages;
+        }
+    }
+}
---
+++ Assets/Scripts/UltimateCipherMode.cs
@@ -0,0 +1,10 @@
+﻿namespace UltimateCipher
+{
+    public enum Mode
+    {
+        Normal, // Three ciphers chosen at random
+        PinkUC, // All non-inverted ciphers
+        CyanUC, // All inverted ciphers
+        TrueUC  // All ciphers
+    }
+}
