6d1a4253e50faf127aca5e9172c24c2dcaa1d804
Timwi <github@timwi.de>
2021-10-25 19:07:18 +0200

    Introducing “Debug mode” for testing ciphers in Ultimate Cipher. Trigger by submitting a single letter. Only available in Unity Editor.

--- Assets/Scripts/UltimateCipherMode.cs
+++ Assets/Scripts/UltimateCipherMode.cs
@@ -5,6 +5,7 @@
         Normal, // Three ciphers chosen at random
         PinkUC, // All non-inverted ciphers
         CyanUC, // All inverted ciphers
-        TrueUC  // All ciphers
+        TrueUC,  // All ciphers
+        Debug   // Can only be triggered in the Unity Editor
     }
 }
